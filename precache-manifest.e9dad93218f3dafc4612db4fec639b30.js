07d804e13152757062a4d5351dba189d1138e3ed
Scott Jones <scott.jones9336@gmail.com>
2023-06-16 13:18:43 +0100

    Updates

---
+++ precache-manifest.e9dad93218f3dafc4612db4fec639b30.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0dde6250b5aa1e20299",
+    "url": "/static/css/main.f6a5c6e4.chunk.css"
+  },
+  {
+    "revision": "d0dde6250b5aa1e20299",
+    "url": "/static/js/main.d0dde625.chunk.js"
+  },
+  {
+    "revision": "73753276c867f0e27edf",
+    "url": "/static/js/1.73753276.chunk.js"
+  },
+  {
+    "revision": "759679d83881bce1d716",
+    "url": "/static/js/2.759679d8.chunk.js"
+  },
+  {
+    "revision": "ee81e1ebcae2b1e9c7a3",
+    "url": "/static/js/runtime~main.ee81e1eb.js"
+  },
+  {
+    "revision": "f9d5c7618a164974542fb3c0d7cd821c",
+    "url": "/static/media/background-1-landscape.f9d5c761.png"
+  },
+  {
+    "revision": "e8e1bf375c7070279153bc4dcbe36299",
+    "url": "/static/media/banner.e8e1bf37.png"
+  },
+  {
+    "revision": "d627fc71dfa5c5720f4db86001fd3982",
+    "url": "/static/media/postreveal-logo.d627fc71.png"
+  },
+  {
+    "revision": "774afe03f42dac9f98da7ea29d22d1ae",
+    "url": "/static/media/prereveal-logo-1.774afe03.png"
+  },
+  {
+    "revision": "541a60eb9fff6bea52c98a46a25ba81b",
+    "url": "/static/media/stone.541a60eb.png"
+  },
+  {
+    "revision": "3174e4ad37582c8f2b2ef3fd4d82f807",
+    "url": "/static/media/Scott.3174e4ad.png"
+  },
+  {
+    "revision": "d627fc71dfa5c5720f4db86001fd3982",
+    "url": "/static/media/christs.d627fc71.png"
+  },
+  {
+    "revision": "cd02569cc0054920adf8619d79d072e7",
+    "url": "/static/media/classico.cd02569c.ttf"
+  },
+  {
+    "revision": "ff77769530617f33807108cea5003ef6",
+    "url": "/index.html"
+  }
+];
